46f7422f14cb0e27712962b94481581c098e8f32
samsmoak <45909225+samsmoak@users.noreply.github.com>
2021-11-16 11:15:20 +0530

    new one

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import API from "./API";
 
 function Register() {
